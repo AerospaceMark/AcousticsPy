b601f9c4d8cf0a71c858bdb7ac00fe3db76f5dc0
Mark C. Anderson <anderson.mark.az@gmail.com>
2022-02-19 16:59:29 -0700

    Improved version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ LONG_DESCRIPTION = 'My first Python package with a slightly longer description'
 # Setting up
 setup(
        # the name must match the folder name 'verysimplemodule'
-        name="acoustics", 
+        name="acousticspy", 
         version=VERSION,
         author="Mark C. Anderson",
         author_email="anderson.mark.az@gmail.com",
